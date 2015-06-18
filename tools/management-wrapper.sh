b5af8172bb01e336d90827a3d420ffd285350475
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-06-18 13:47:13 +0300

    Add local configuration to wrapper.

--- tools/management-wrapper.sh
+++ tools/management-wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
 
+[ -r /etc/default/digabi-repository-server ] && . /etc/default/digabi-repository-server
+
 sudo -u digabi-repository /usr/lib/digabi-repository/repository-management.sh $@
